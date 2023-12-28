400d50c5f3c85431076f6eac5c2c20493669e2fc
Azimbek <omar.azimbek@gmail.com>
2023-12-28 14:47:39 +0600

    0.3.5 Commit

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+    static void divide(int toBeDivided) {
+        float result = (float) toBeDivided / 5;
+        System.out.println(result);
+    }
+    public static void main(String[] args) {
+        divide(23);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
-    public void convert(float minutes) {
+    public static void main(String[] args) {
+        convert(8.5F);
+    }
+    public static void convert(float minutes) {
+        float seconds = minutes * 60;
+        System.out.println(seconds);
     }
 }
